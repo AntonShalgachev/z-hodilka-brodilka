54799df01703de74584d89a42b3f14604fae39bd
Anton Shalgachev <anton.shalgachev@gmail.com>
2016-09-03 20:37:07 +0300

    Added bee and spider prefab

---
+++ Assets/Scripts/BeeController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeeController : MonoBehaviour
+{
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+	void OnAnimationEnd()
+	{
+		Debug.Log("Bee got slaughtered");
+		Destroy(gameObject);
+	}
+}
