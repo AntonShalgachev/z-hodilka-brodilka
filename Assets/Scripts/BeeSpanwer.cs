b6371ff4b6cb9cc3ce194bdded28d6b601a3773d
Anton Shalgachev <anton.shalgachev@gmail.com>
2016-09-04 16:06:21 +0300

    Working version

--- Assets/Scripts/BeeSpanwer.cs
+++ Assets/Scripts/BeeSpanwer.cs
@@ -9,14 +9,17 @@ public class BeeSpanwer : NetworkBehaviour
 	[SerializeField]
 	private int numberOfBees;
 
+	[SerializeField]
+	private TerrainGenerator terrain;
+
 	public override void OnStartServer()
 	{
 		base.OnStartServer();
 
-		var terrainObject = GameObject.Find("Terrain");
-		Debug.Assert(terrainObject != null, "Can't find Terrain object");
-		var terrain = terrainObject.GetComponent<TerrainGenerator>();
-		Debug.Assert(terrain != null, "Terrain doesn't contain TerrainGenerator");
+		//var terrainObject = GameObject.Find("Terrain");
+		//Debug.Assert(terrainObject != null, "Can't find Terrain object");
+		//var terrain = terrainObject.GetComponent<TerrainGenerator>();
+		//Debug.Assert(terrain != null, "Terrain doesn't contain TerrainGenerator");
 
 		var terrainRect = terrain.GetRect();
 		
@@ -30,16 +33,8 @@ public class BeeSpanwer : NetworkBehaviour
 			var position = new Vector2(Random.Range(xfrom, xto), Random.Range(yfrom, yto));
 			var bee = Object.Instantiate(beePrefab, position, Quaternion.identity) as GameObject;
 			NetworkServer.Spawn(bee);
-		}
-	}
 
-	void Start ()
-	{
-		
-	}
-	
-	void Update ()
-	{
-		
+			bee.transform.parent = transform;
+		}
 	}
 }
---
+++ Assets/Scripts/SpawnGenerator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class SpawnGenerator : NetworkBehaviour
+{
+	[SerializeField]
+	private GameObject spawnPrefab;
+	[SerializeField]
+	private int numberOfSpawns;
+
+	[SerializeField]
+	private TerrainGenerator terrain;
+
+	public override void OnStartServer()
+	{
+		base.OnStartServer();
+
+		var terrainRect = terrain.GetRect();
+
+		var xfrom = terrainRect.xMin;
+		var xto = terrainRect.xMax;
+		var yfrom = terrainRect.yMin;
+		var yto = terrainRect.yMax;
+
+		for (var i = 0; i < numberOfSpawns; i++)
+		{
+			var position = new Vector2(Random.Range(xfrom, xto), Random.Range(yfrom, yto));
+			var spawnPos = Object.Instantiate(spawnPrefab, position, Quaternion.identity) as GameObject;
+			spawnPos.transform.parent = transform;
+		}
+	}
+}
